7fcefcc8afeb6ab8332f31756b7fc8a86cf31fb4
AppsLeloTsf <appslelo.com@gmail.com>
2018-12-04 11:08:32 +0530

    Initial commit

---
+++ app/src/main/java/com/videocall/tsfchat/MainActivity.java
@@ -0,0 +1,28 @@
+package com.videocall.tsfchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.textView)
+    TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        textView.setText("Hello Tousif Akram");
+    }
+}
